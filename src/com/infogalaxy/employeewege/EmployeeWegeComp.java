2255b54c2f0a1a8553ce2360f00f34725e06a956
harshaltamboli <harshaltamboli429@gmail.com>
2024-03-05 08:47:49 +0530

    [MODIFIED]:Harshal-Uc 1 computation for Emplyee Attendance

--- src/com/infogalaxy/employeewege/EmployeeWegeComp.java
+++ src/com/infogalaxy/employeewege/EmployeeWegeComp.java
@@ -1,5 +1,28 @@
+
 package com.infogalaxy.employeewege;
 
-public class EmployeeWegeComp {
+import java.util.Scanner;
 
+public class EmployeeWegeComp {
+	public static void main(String args[]) {
+		
+		int attendance=0;
+		Scanner sc=new Scanner(System.in);	
+	System.out.println("Welcome To Employee Wage omputation program");
+	
+	System.out.println("Enter The Attendance : [0- Absent / 1- present]");
+	attendance = sc.nextInt();
+	if(attendance==0) {
+		
+		System.out.println("Employee Is Absent ");
+		
+	}
+	
+	else{
+		
+		System.out.println("Employee is present ");
+		
+	}
+
+}
 }
