bb32417fa81ac96beb8bbcfa4bda4400ecc25c49
harshaltamboli <harshaltamboli429@gmail.com>
2024-03-07 12:39:36 +0530

    [MODIFIED]: harshal-Added Emloyee type check functionlity

--- src/com/infogalaxy/employeewege/EmployeeWegeComp.java
+++ src/com/infogalaxy/employeewege/EmployeeWegeComp.java
@@ -7,10 +7,12 @@ import java.util.Random;
 public class EmployeeWegeComp {
 	public static void main(String args[]) {
 		
-		int attendance = 0;
+		int attendance ;
 		int dailyWage  = 0;
+		int employee_type ;
 		final int WAGE_PER_HR = 20;
 		final int FULL_DAY_HR = 8;
+		final int PART_TIME_HR = 4;
 		System.out.println("Welcome To Employee Wage omputation program");
 	
 		Random random = new Random();
@@ -28,7 +30,20 @@ public class EmployeeWegeComp {
 	else{
 		
 		System.out.println("Employee is present ");
-		dailyWage  = WAGE_PER_HR *FULL_DAY_HR;
+	
+		employee_type = random.nextInt(9) % 2 ;
+		
+		if(employee_type == 0) {
+			System.out.println("Employee is part time");
+			dailyWage  = WAGE_PER_HR *PART_TIME_HR;
+		}
+		
+		else {
+			
+			System.out.println("Employee is full time");
+			dailyWage  = WAGE_PER_HR *FULL_DAY_HR;
+		}
+		//dailyWage  = WAGE_PER_HR *FULL_DAY_HR;
 		System.out.println("Employee Wage="+dailyWage);
 	}
 
