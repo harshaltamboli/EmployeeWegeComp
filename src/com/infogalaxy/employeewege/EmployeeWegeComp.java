60b1658bf7daf73b1146b05729feb1415c12c457
harshaltamboli <harshaltamboli429@gmail.com>
2024-03-07 12:06:36 +0530

    [MODIFIED]: harshal -Calculate DailyWae if Employe present

--- src/com/infogalaxy/employeewege/EmployeeWegeComp.java
+++ src/com/infogalaxy/employeewege/EmployeeWegeComp.java
@@ -7,16 +7,16 @@ import java.util.Random;
 public class EmployeeWegeComp {
 	public static void main(String args[]) {
 		
-		int attendance=0;
+		int attendance = 0;
+		int dailyWage  = 0;
+		final int WAGE_PER_HR = 20;
+		final int FULL_DAY_HR = 8;
 		System.out.println("Welcome To Employee Wage omputation program");
 	
 		Random random = new Random();
 		attendance=random.nextInt(9)%2;
 		
-	//	attendance=Math.random();
-
-
-		System.out.println("Random now:"+Math.floor(Math.random() %2));
+	
 		
 		System.out.println("Random now:"+attendance);
 	if(attendance==0) {
@@ -28,7 +28,8 @@ public class EmployeeWegeComp {
 	else{
 		
 		System.out.println("Employee is present ");
-		
+		dailyWage  = WAGE_PER_HR *FULL_DAY_HR;
+		System.out.println("Employee Wage="+dailyWage);
 	}
 
 }
