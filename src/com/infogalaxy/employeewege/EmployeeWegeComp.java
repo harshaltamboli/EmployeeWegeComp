3a5bbf77b9b0b6ff5be56fba7fe1050e6a6f0a42
harshaltamboli <harshaltamboli429@gmail.com>
2024-03-09 14:00:10 +0530

    [MODIFIED]: harshal - modified code for class and object

--- src/com/infogalaxy/employeewege/EmployeeWegeComp.java
+++ src/com/infogalaxy/employeewege/EmployeeWegeComp.java
@@ -5,17 +5,18 @@ import java.util.Random;
 
 
 public class EmployeeWegeComp {
-	public static void main(String args[]) {
-		int attendance ;
-		int dailyWage  = 0;
-		int employee_type ;
-		int total_working_hr = 0;
-		int total_working_days =1;
-		int total_wage =0;
-		final int WAGE_PER_HR = 20;
-		final int FULL_TIME_HR = 8;
-		final int PART_TIME_HR = 4;
-		final int WORKING_DAYS =20;
+	int attendance ;
+	int dailyWage  = 0;
+	int employee_type ;
+	int total_working_hr = 0;
+	int total_working_days =1;
+	int total_wage =0;
+	final int WAGE_PER_HR = 20;
+	final int FULL_TIME_HR = 8;
+	final int PART_TIME_HR = 4;
+	final int WORKING_DAYS =20;
+	public void  employeeWageCalce() {
+		
 		System.out.println("Welcome To Employee Wage omputation program");
 		
 		while(total_working_hr <= 100 && total_working_days <= 20) {
@@ -67,7 +68,13 @@ public class EmployeeWegeComp {
 			total_working_days++;
 		}
 		System.out.println("Employee Monthly Wage="+total_wage);
+	}
 		
+		public static void main(String args[]) {
+			EmployeeWegeComp employeeWegeComp = new EmployeeWegeComp();
+			
+			employeeWegeComp.employeeWageCalce();		
+	}
 	
-		}
-}
+	
+	}
